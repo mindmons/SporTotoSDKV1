19b44c57198e08170aa4ace68d3f2a0836d4d301
mindmons <ismailkaracayiir@gmail.com>
2025-09-15 15:06:32 +0300

    edit packege

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "SporTotoSDK",
+    name: "SporTotoSDKV1",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: "SporTotoSDK",
-            targets: ["SporTotoSDK"]
+            name: "SporTotoSDKV1",
+            targets: ["SporTotoSDKV1"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "SporTotoSDK",
+            name: "SporTotoSDKV1",
             url: "https://github.com/mindmons/SporTotoSDKV1/releases/download/v1.0.0/SporTotoSDKV1.xcframework.zip",
             checksum: "7a5600b49f3386daad2a4aaab1244c09fc837364c89e496d642ba44d7f82f864"
         )
